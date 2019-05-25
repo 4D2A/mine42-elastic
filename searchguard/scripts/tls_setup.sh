ea496105420ec0c9d0c5ebeb9a98257ec261719d
4D2A <4D2A@users.noreply.github.com>
2019-05-25 12:40:00 +0200

    Integrating with Elasticsearch certutil

--- searchguard/scripts/tls_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source `dirname $0`/common.sh
-
-
-loginfo "setting-up Search Guard TLS: TLSTOOL_CONFIG='${TLSTOOL_CONFIG}', TLSTOOL_OUTPUT='${TLSTOOL_OUTPUT}'"
-${TLSTOOL_ROOT}/tools/sgtlstool.sh -c ${TLSTOOL_CONFIG} -crt -ca -t ${TLSTOOL_OUTPUT}
